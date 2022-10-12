a2b36a32f4361505a91488e620fc170ebc2f5306
Valentin <valentin.remy88@gmail.com>
2022-10-12 13:39:06 +0200

    change permission

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost"],
+    domains: ["cv-beta-eight.vercel.app"],
   },
   typescript: {
     // !! WARN !!
