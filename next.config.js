b0b15a3590d7f77849389432c567b0cc4baba017
Valentin <valentin.remy88@gmail.com>
2022-10-12 13:41:47 +0200

    change ip

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["cv-beta-eight.vercel.app"],
+    domains: ["https://cv-beta-eight.vercel.app/"],
   },
   typescript: {
     // !! WARN !!
